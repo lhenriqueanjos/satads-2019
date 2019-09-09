e9288743c1ffa0bddf51384cb9b1d8883d0f5c3c
Travis CI <lhenriqueanjos@gmail.com>
2019-09-09 14:33:45 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.78fdfb4b1c21493f23c72c3e8120d7e3.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/satads-2019/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/satads-2019/robots.txt"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/satads-2019/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "e19523605b675569378e",
+    "url": "/satads-2019/js/chunk-vendors.8895b344.js"
+  },
+  {
+    "revision": "73599fcf206d29b838c8",
+    "url": "/satads-2019/js/recomendacoes.6592e0f8.js"
+  },
+  {
+    "revision": "7901c9ed49796fe40788",
+    "url": "/satads-2019/js/referencias.14a74669.js"
+  },
+  {
+    "revision": "78c2d97ca1945a88fc6d",
+    "url": "/satads-2019/js/sobre.d91a08fa.js"
+  },
+  {
+    "revision": "d4fdc099ef34e14e663f",
+    "url": "/satads-2019/js/home.4b48dfa8.js"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/satads-2019/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "cefe68c0b33f11e42164",
+    "url": "/satads-2019/js/app.a582d8ee.js"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/satads-2019/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "ff13d121c1a1cf74d8e06bd39e1746c3",
+    "url": "/satads-2019/fonts/materialdesignicons-webfont.ff13d121.woff"
+  },
+  {
+    "revision": "b62dee6d970fe28d56766e63cfb18935",
+    "url": "/satads-2019/img/luiz.b62dee6d.jpeg"
+  },
+  {
+    "revision": "a8803c0a15d25a909efd3ab48f61eb89",
+    "url": "/satads-2019/img/satads.a8803c0a.jpg"
+  },
+  {
+    "revision": "82d5e4c4e3cfbf3952442b407acb85ae",
+    "url": "/satads-2019/index.html"
+  },
+  {
+    "revision": "53f47f15f3996aff7b1d489a50157b04",
+    "url": "/satads-2019/img/cap.53f47f15.jpg"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/satads-2019/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "541e65fb46e44ecdf7a9da8a9b881446",
+    "url": "/satads-2019/fonts/materialdesignicons-webfont.541e65fb.eot"
+  },
+  {
+    "revision": "fc03f7f15facede623faa7666c7d1f5a",
+    "url": "/satads-2019/fonts/materialdesignicons-webfont.fc03f7f1.ttf"
+  },
+  {
+    "revision": "c61b9c12f68ee1ba045a4b49dba29ca5",
+    "url": "/satads-2019/fonts/materialdesignicons-webfont.c61b9c12.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/satads-2019/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/satads-2019/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/satads-2019/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/satads-2019/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/satads-2019/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/satads-2019/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/satads-2019/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/satads-2019/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/satads-2019/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/satads-2019/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/satads-2019/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/satads-2019/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/satads-2019/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/satads-2019/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/satads-2019/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/satads-2019/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/satads-2019/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/satads-2019/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/satads-2019/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "e19523605b675569378e",
+    "url": "/satads-2019/css/chunk-vendors.bbf0e298.css"
+  }
+];
