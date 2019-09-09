5c24dda3cba20edb34b342b12afe7109ccd51ad8
Luiz Henrique Nascimento dos Anjos <luiz.anjos@contabilizei.com.br>
2019-09-08 21:26:57 -0300

    botao de voltar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
+      <v-btn text icon color="white" v-if="$router.currentRoute.name !== 'home'" @click="goBack">
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+
       <v-toolbar-title class="headline">
-        <span>
-          Web, SPA, Vue.js - SATADS 19
-        </span>
+        <span>Web, SPA, Vue.js - SATADS 19</span>
       </v-toolbar-title>
     </v-app-bar>
 
@@ -18,8 +20,10 @@
 
 export default {
   name: 'App',
-  data: () => ({
-    //
-  }),
+  methods: {
+    goBack() {
+      window.history.back();
+    },
+  },
 };
 </script>
